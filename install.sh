a2d0ad93d1943aa2695f63521722bc6b2bc3b7d9
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-26 11:44:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,13 @@ apt-get -y install tinyproxy \
     && rm -rf /etc/tinyproxy/*
 
 # install sonarr
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
-echo "deb http://apt.sonarr.tv/ master main" | tee /etc/apt/sources.list.d/sonarr.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 \
+    && echo "deb http://apt.sonarr.tv/ master main" | tee /etc/apt/sources.list.d/sonarr.list
 apt-get -y install nzbdrone
+
+# install radarr
+cd /tmp \
+    && curl -L -O $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 ) \
+    && tar -xvzf Radarr.develop.*.linux.tar.gz \
+    && rm -f Radarr.develop.*.linux.tar.gz \
+    && mv Radarr /opt 
