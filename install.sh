648306622df458b504d37446c7333707386be4e0
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-27 13:27:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,34 +3,46 @@
 # install more packages
 apt-get -y update \
     && apt-get -y install apt-transport-https \
-    && apt-get -y install --no-install-recommends --no-install-suggests wget curl unzip dnsutils sipcalc mediainfo sqlite3 jq gnupg gnupg2 gnupg1
-
-# install openvpn with nftables
-apt-get install -y openvpn
-apt-get install -y nftables
-
-# install stubby and clean config
-apt-get -y install stubby \
-    && mkdir -p /etc/stubby \
-    && rm -rf /etc/stubby/*
-
-# install dante server and clean config
-apt-get -y install dante-server \
-    && rm -f /etc/danted.conf
-
-# install tinyproxy and clean config
-apt-get -y install tinyproxy \
-    && mkdir -p /etc/tinyproxy \
-    && rm -rf /etc/tinyproxy/*
+    && apt-get -y install --no-install-recommends --no-install-suggests bzip2 ca-certificates-mono libcurl4-openssl-dev mediainfo mono-devel mono-vbnc python sqlite3 unzip \
+    && apt-get -y install wget curl dnsutils sipcalc jq gnupg gnupg2 gnupg1 libicu60 libssl1.0
 
 # install sonarr
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 \
-    && echo "deb http://apt.sonarr.tv/ master main" | tee /etc/apt/sources.list.d/sonarr.list
-apt-get -y install nzbdrone
+SONARR_VERSION=$(curl -sX GET https://services.sonarr.tv/v1/download/master | jq -r '.version')
+curl -o /tmp/sonarr.tar.gz -L "https://download.sonarr.tv/v2/master/mono/NzbDrone.master.${SONARR_VERSION}.mono.tar.gz" \
+    && mkdir -p /app/sonarr \
+    && tar xf /tmp/sonarr.tar.gz -C /app/sonarr --strip-components=1 \
+    && rm -f /tmp/sonarr.tar.gz \
+    && echo "$(date "+%d.%m.%Y %T") Added sonarr version ${SONARR_VERSION}" >> /build_date.info
 
 # install radarr
-cd /tmp \
-    && curl -L -O $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 ) \
-    && tar -xvzf Radarr.develop.*.linux.tar.gz \
-    && rm -f Radarr.develop.*.linux.tar.gz \
-    && mv Radarr /opt 
+RADARR_RELEASE=$(curl -sX GET "https://api.github.com/repos/Radarr/Radarr/releases" | jq -r '.[0] | .tag_name')
+radarr_url=$(curl -s https://api.github.com/repos/Radarr/Radarr/releases/tags/"${RADARR_RELEASE}" | jq -r '.assets[].browser_download_url' | grep linux)
+curl -o /tmp/radar.tar.gz -L "${radarr_url}" \
+    && mkdir -p /app/radarr \
+    && tar ixzf /tmp/radar.tar.gz -C /app/radarr --strip-components=1 \
+    && rm -f /tmp/radar.tar.gz \
+    && echo "$(date "+%d.%m.%Y %T") Added radarr version ${RADARR_RELEASE}" >> /build_date.info
+
+# install jackett
+JACKETT_RELEASE=$(curl -sX GET "https://api.github.com/repos/Jackett/Jackett/releases/latest" | jq -r .tag_name)
+curl -o /tmp/jacket.tar.gz -L "https://github.com/Jackett/Jackett/releases/download/${JACKETT_RELEASE}/Jackett.Binaries.LinuxAMDx64.tar.gz" \
+    && mkdir -p /app/Jackett \
+    && tar xf /tmp/jacket.tar.gz -C /app/Jackett --strip-components=1 \
+    && chown -R root:root /app/Jackett \
+    && rm -f /tmp/jacket.tar.gz \
+    && echo "$(date "+%d.%m.%Y %T") Added jackett version ${JACKETT_RELEASE}" >> /build_date.info
+
+# install openvpn base
+curl -o /install_openvpn.sh -L "https://raw.githubusercontent.com/testdasi/openvpn-client-aio-base/master/install.sh"
+RUN /bin/bash /install_openvpn.sh \
+    && rm -f /install_openvpn.sh
+
+# install hydesa base
+curl -o /install_hydesa.sh -L "https://raw.githubusercontent.com/testdasi/openvpn-hydesa-base/master/install.sh"
+RUN /bin/bash /install_hydesa.sh \
+    && rm -f /install_hydesa.sh
+
+# install hyrosa base
+curl -o /install_hyrosa.sh -L "https://raw.githubusercontent.com/testdasi/openvpn-hyrosa-base/master/install.sh"
+RUN /bin/bash /install_hyrosa.sh \
+    && rm -f /install_hyrosa.sh
