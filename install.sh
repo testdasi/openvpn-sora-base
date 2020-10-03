5b8fcf29ce1eb9f259af6cbf85967f14d2ab072a
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 20:33:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,9 +26,9 @@ curl -o /tmp/radar.tar.gz -L "${radarr_url}" \
 # install jackett
 JACKETT_RELEASE=$(curl -sX GET "https://api.github.com/repos/Jackett/Jackett/releases/latest" | jq -r .tag_name)
 curl -o /tmp/jacket.tar.gz -L "https://github.com/Jackett/Jackett/releases/download/${JACKETT_RELEASE}/Jackett.Binaries.LinuxAMDx64.tar.gz" \
-    && mkdir -p /app/Jackett \
-    && tar xf /tmp/jacket.tar.gz -C /app/Jackett --strip-components=1 \
-    && chown -R root:root /app/Jackett \
+    && mkdir -p /app/jackett \
+    && tar xf /tmp/jacket.tar.gz -C /app/jackett --strip-components=1 \
+    && chown -R root:root /app/jackett \
     && rm -f /tmp/jacket.tar.gz \
     && echo "$(date "+%d.%m.%Y %T") Added jackett version ${JACKETT_RELEASE}" >> /build_date.info
 
