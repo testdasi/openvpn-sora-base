362fa095940bf3fb65f60f2f21c199a548fd5161
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-13 08:59:46 +0000

    Update install_tor.sh

--- install_tor.sh
+++ install_tor.sh
@@ -14,22 +14,15 @@ apt-get -y update \
     && apt-get -y install rtorrent screen
     
 # install additional packages to build flood
-apt-get -y install build-essential git
+# apt-get -y install build-essential git
 # install nodejs to run flood
 curl -sL https://deb.nodesource.com/setup_lts.x | bash - \
     && apt-get -y install -y nodejs
-# build flood
-# cd /app \
-#    && git clone https://github.com/jfurrow/flood.git \
-#    && cd flood \
-#    && cp config.template.js config.js \
-#    && npm install \
-#    && npm install -g node-gyp \
-#    && npm run build
 # New fork of flood at https://github.com/jesec/flood
 npm install --global flood
 
 # clean up building packages (and reinstall procps which is somehow removed by autoremove)
-apt-get -y remove build-essential git \
-    && apt-get -y autoremove \
+# apt-get -y remove build-essential git \
+apt-get -y autoremove \
+#    && apt-get -y autoremove \
     && apt-get -y install procps
