2d8d198ece4689b37ebb40044903122901a89a91
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-15 14:39:48 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ source /install_openvpn.sh
 source /install_nzb.sh
 source /install_tor.sh
 
-## Sonarr and Radarr section ##
+## mono-arr section ##
 # add mono repo
 apt-get -y update \
     && apt-get -y install apt-transport-https dirmngr gnupg gnupg2 gnupg1 ca-certificates \
@@ -25,14 +25,23 @@ curl -o /tmp/sonarr.tar.gz -L "https://download.sonarr.tv/v3/main/${SONARR_VERSI
     && mkdir -p /app/sonarr \
     && tar xf /tmp/sonarr.tar.gz -C /app/sonarr --strip-components=1 \
     && rm -f /tmp/sonarr.tar.gz \
-    && echo "$(date "+%d.%m.%Y %T") Added sonarr version ${SONARR_VERSION}" >> /build_date.info
+    && echo "$(date "+%d.%m.%Y %T") Added sonarr version ${SONARR_VERSION} from master branch" >> /build_date.info
 # install radarr
 RADARR_RELEASE=$(curl -sL "https://radarr.servarr.com/v1/update/master/changes?os=linux&runtime=netcore&arch=x64" | jq -r '.[0].version')
 curl -o /tmp/radarr.tar.gz -L "https://radarr.servarr.com/v1/update/master/updatefile?version=${RADARR_RELEASE}&os=linux&runtime=netcore&arch=x64" \
     && mkdir -p /app/radarr \
     && tar ixzf /tmp/radarr.tar.gz -C /app/radarr --strip-components=1 \
     && rm -f /tmp/radarr.tar.gz \
-    && echo "$(date "+%d.%m.%Y %T") Added radarr version ${RADARR_RELEASE}" >> /build_date.info
+    && echo "$(date "+%d.%m.%Y %T") Added radarr version ${RADARR_RELEASE} from master branch" >> /build_date.info
+# install prowlarr - will use master branch once prowlarr stable is available
+PROWLARR_BRANCH="develop"
+PROWLARR_RELEASE=$(curl -sL "https://prowlarr.servarr.com/v1/update/${PROWLARR_BRANCH}/changes?runtime=netcore&os=linux" | jq -r '.[0].version')
+curl -o /tmp/prowlarr.tar.gz -L "https://prowlarr.servarr.com/v1/update/${PROWLARR_BRANCH}/updatefile?version=${PROWLARR_RELEASE}&os=linux&runtime=netcore&arch=x64" \
+    && mkdir -p /app/prowlarr \
+    && tar ixzf /tmp/prowlarr.tar.gz -C /app/prowlarr --strip-components=1 \
+    && rm -f /tmp/prowlarr.tar.gz \
+    && rm -rf /app/prowlarr/prowlarr.Update \
+    && echo "$(date "+%d.%m.%Y %T") Added prowlarr version ${RADARR_RELEASE} from ${PROWLARR_BRANCH} branch" >> /build_date.info
 
 ## Remaining apps ##
 # install torsocks and privoxy
@@ -50,8 +59,6 @@ cd /tmp \
     && chmod +x /app/launcher/launcher-python2.sh \
     && chmod +x /app/launcher/launcher-python3.sh \
     && rm -f /tmp/launcher.zip
-# install cloudcmd
-# npm i cloudcmd -g
 
 ## Clean up ##
 apt-get -y autoremove \
